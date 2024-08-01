7c22d90995402bb1f12d93787ff39ba063ef4d5b
Bryn Ellison <bryn.ellison@outlook.com>
2024-08-01 17:57:48 +0100

    Add Create suggestion page

---
+++ SuggestionAppUI/Models/CreateSuggestionModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SuggestionAppUI.Models;
+
+public class CreateSuggestionModel
+{
+    [Required]
+    [MaxLength(75)]
+    public string Suggestion { get; set; }
+    [MinLength(1)]
+    [DisplayName("Category")]
+    public string CategoryId { get; set; }
+    [MaxLength(500)]
+    public string Description { get; set; }
+
+}
